f96bb521f23fd05af03ebc93c705f604ec452806
Jaya Iskandar <jayaiskandar17@gmail.com>
2024-04-27 21:41:45 +0700

    Using Single Endpoint /dashContent, Added Model graph

---
+++ src/components/ModelChart/ModelChart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ModelPlotChart from '../ModelPlotChart/ModelPlotChart';
+
+const ModelChart = () => {
+  return (
+    <div className="one-chart">
+      <ModelPlotChart/>
+    </div>
+  );
+};
+
+export default ModelChart;
